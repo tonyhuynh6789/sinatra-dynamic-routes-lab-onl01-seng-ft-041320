07b4fd7e2902e53fa74d914a5d32cb00774e1637
Tony Rim <duc_thinh_03@yahoo.com>
2020-06-09 01:50:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,8 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do 
      @number = params[:number].to_i
-    @phrase = params[:phrase]
+     @phrase = params[:phrase]
+     erb :say
   end 
  
 
